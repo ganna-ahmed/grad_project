7eb96bb950bf775115e30421386cf110bded8dc4
Ganna Ahmed <gannaahmed5432@gmail.com>
2024-10-22 09:10:01 +0300

    adding color file

---
+++ lib/core/constant/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color red = Color(0xffB8001F);
+  static const Color darkSlateBlue = Color(0xff384B70);
+  static const Color steelBlue = Color(0xff507687);
+  static const Color snow = Color(0xffFFF8F8);
+  static const Color white = Color(0xffffffff);
+}
